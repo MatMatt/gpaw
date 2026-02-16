0999807c35c13f7b6088383ca1f033c328b50986
Giulia Gamboni <giulia.gamboni@phd.units.it>
2026-02-16 18:00:04 +0100

    fix PEP8

--- gpaw/new/etdm/edmiston_ruedenberg/obj_function.py
+++ gpaw/new/etdm/edmiston_ruedenberg/obj_function.py
@@ -12,7 +12,8 @@ import numpy as np
 
 
 class EdmistonRuedenberg(ObjectiveFunctionETDM):
-    def __init__(self, ibzwfs, loctype="pseudo-paw", indices="all", representation="full"):
+    def __init__(self, ibzwfs, loctype="pseudo-paw", indices="all", 
+                 representation="full"):
 
         dtype, nkps = (
             ibzwfs.dtype,
@@ -132,7 +133,8 @@ class EdmistonRuedenberg(ObjectiveFunctionETDM):
 
 
 class EdmistonRuedenbergUpdateRef(EdmistonRuedenberg):
-    def __init__(self, ibzwfs, loctype="pseudo-paw", indices="all", representation="full"):
+    def __init__(self, ibzwfs, loctype="pseudo-paw", indices="all", 
+                 representation="full"):
         super().__init__(ibzwfs, loctype, indices, representation)
 
     def rotate_wfs(self):
