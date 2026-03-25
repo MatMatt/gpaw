ed886d135a19980cb6e2844ed18ac57fb6733c0e
Silvan Kretschmer <silkr@dtu.dk>
2026-03-25 16:22:28 +0100

    Added h2o_ecut.py and added to agts.py

--- doc/tutorialsexercises/basics/representation/agts.py
+++ doc/tutorialsexercises/basics/representation/agts.py
@@ -3,13 +3,23 @@ import numpy as np
 
 
 def test():
-    txt = pathlib.Path('results-0.20.txt').read_text()
+    txt = pathlib.Path('results-400.txt').read_text()
     last_line = txt.split('\n')[-2]
     atomization_energy = float(last_line)
     assert np.isclose(atomization_energy, -11.5211)
+
+
+def test_ecut():
+    txt = pathlib.Path('results-500.txt').read_text()
+    last_line = txt.split('\n')[-2]
+    atomization_energy = float(last_line)
+    assert np.isclose(atomization_energy, -11.6263)
 
 
 def workflow():
     from myqueue.workflow import run
     with run(script='h2o.py'):
         run(function=test)
+
+    with run(script='h2o_ecut.py'):
+        run(function=test_ecut)
