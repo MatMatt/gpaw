c4704ea1a4f1293aee29764cdd329fa8c8e43ee8
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-04-08 12:39:59 +0200

    Fix LCAO-GW

--- doc/tutorialsexercises/opticalresponse/gw_tutorial/C_lcao_gw.py
+++ doc/tutorialsexercises/opticalresponse/gw_tutorial/C_lcao_gw.py
@@ -1,14 +1,18 @@
-from gpaw.response.g0w0 import G0W0
 import numpy as np
+from gpaw.new.calculation import DFTCalculation
+from gpaw.response.g0w0 import G0W0
 
-gw = G0W0('C_lcao_groundstate.gpw',
+dft = DFTCalculation.from_gpw_file('C_lcao_groundstate.gpw')
+dft.change_mode('pw')
+dft.write_gpw_file('C_pw_from_lcao_groundstate.gpw', include_wfs=True)
+
+gw = G0W0('C_pw_from_lcao_groundstate.gpw',
           integrate_gamma='WS',
           ecut=200,
           kpts=[0],
           eta=0.1,
           bands=(3, 5),
           evaluate_sigma=np.linspace(-50, 75, 500),
-          filename='C-g0w0-lcao'
-          )
+          filename='C-g0w0-lcao')
 
 gw.calculate()
