d143d09a8bdd959ac9bd028b4dcc81b214f912aa
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-04-20 17:14:21 +0200

    Get rid of GPAW_NEW

--- doc/tutorialsexercises/electrostatics/jellium/surface.py
+++ doc/tutorialsexercises/electrostatics/jellium/surface.py
@@ -1,8 +1,11 @@
+import json
+from pathlib import Path
 import numpy as np
 from ase import Atoms
 from ase.units import Bohr
-from gpaw.jellium import JelliumSlab
 from gpaw import GPAW, PW
+from gpaw.core import UGArray
+from gpaw.new.extensions import Jellium
 
 rs = 5.0 * Bohr  # Wigner-Seitz radius
 h = 0.2          # grid-spacing
@@ -14,12 +17,20 @@ k = 12           # number of k-points (k*k*1)
 ne = a**2 * L / (4 * np.pi / 3 * rs**3)
 
 eps = 0.001  # displace surfaces away from grid-points
-jellium = JelliumSlab(ne, z1=v - eps, z2=v + L + eps)
+
+
+class JelliumSlab(Jellium):
+    def update_mask(self, mask_r: UGArray) -> None:
+        z = mask_r.desc.xyz()[:, :, :, 2]
+        mask_r.data[np.logical_and(z > v - eps, z < v + L + eps)] = 1.0
+
 
 surf = Atoms(pbc=(True, True, False),
              cell=(a, a, v + L + v))
 surf.calc = GPAW(mode=PW(400.0),
-                 background_charge=jellium,
+                 extensions=[JelliumSlab(charge=ne)],
+                 # background_charge=jellium,
+                 charge=-ne,
                  xc='LDA_X+LDA_C_WIGNER',
                  kpts=[k, k, 1],
                  h=h,
@@ -27,4 +38,5 @@ surf.calc = GPAW(mode=PW(400.0),
                  nbands=int(ne / 2) + 15,
                  txt='surface.txt')
 e = surf.get_potential_energy()
-surf.calc.write('surface.gpw')
+density = surf.calc.get_pseudo_density()[0, 0]
+Path('surface.json').write_text(json.dumps(density.tolist()))
