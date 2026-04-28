58f258cf0556f322b3732213cd830a584f5d4184
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-04-28 13:24:48 +0200

    Allow number of iterations to decrease

--- doc/documentation/directmin/tools_and_data.py
+++ doc/documentation/directmin/tools_and_data.py
@@ -50,14 +50,14 @@ def get_energy_and_iters(atoms, dm):
 
 def compare_calculated_and_saved_results(e_diff, iters_diff,
                                          eig_string, name, dm):
-    if e_diff > 1.0e-2:
+    if abs(e_diff) > 1.0e-2:
         warnings.warn('Absolute difference in total energy '
                       'for ' + eig_string[dm] + ' calculation of '
                       + name + ' with respect to saved results '
                       'is %f eV'
                       % e_diff)
     if iters_diff > 3:
-        warnings.warn('Absolute difference in total number of '
+        warnings.warn('Difference in total number of '
                       'iterations for ' + eig_string[dm] +
                       ' calculation of ' + name + ' with respect '
                       'to saved results is %d'
