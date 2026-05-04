4ffe06f46abe70a52b1559f6c3126bc86b60e643
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-05-04 12:25:43 +0200

    non self-consistent off-diagonal elements of EXX operator

--- gpaw/core/plane_waves.py
+++ gpaw/core/plane_waves.py
@@ -337,6 +337,7 @@ class PWArray(XArray[PWDesc]):
                           data=data)
 
     def new(self,
+            *,
             data=None,
             dims=None) -> PWArray:
         """Create new PWArray object of same kind.
