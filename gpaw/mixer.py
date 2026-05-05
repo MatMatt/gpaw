91d1d9b0ba8b16b04b8b952ab5532c5aefdfa273
the-ohm <mikhik@yahoo.dk>
2026-05-05 23:36:20 +0200

    Testing MSR1 as default

--- gpaw/mixer.py
+++ gpaw/mixer.py
@@ -1185,12 +1185,12 @@ def get_mixer_from_keywords(pbc, nspins, **mixerkwargs):
 
     # The plan is to first establish a kwargs dictionary with all the
     # defaults, then we update it with values from the user.
-    kwargs = {'backend': BaseMixer}
+    kwargs = {'backend': MSR1Mixer}
 
     if np.any(pbc):  # Works on array or boolean
-        kwargs.update(beta=0.08, history=16, weight=70.0)
+        kwargs.update(beta=0.05, history=10, weight=70.0)
     else:
-        kwargs.update(beta=0.25, history=16, weight=1.0)
+        kwargs.update(beta=0.2, history=10, weight=1.0)
 
     if nspins == 1:
         kwargs['method'] = SeparateSpinMixerDriver
