cce3a7715a1847e2a84484c53f0ab89a39a0d7c9
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-05-20 14:11:55 +0200

    TYPO

--- doc/summerschools/summerschool26/batteries/agts.py
+++ doc/summerschools/summerschool26/batteries/agts.py
@@ -16,7 +16,7 @@ def workflow():
     s21 = run(script='solution2-1.py', tmax='1h', cores=8)
     s22 = run(script='solution2-2.py', tmax='1h', cores=8)
     with run(script='solution3.py', tmax='1h', cores=8, deps=[s21, s22]):
-            run(script='solution4.py', tmax='1h', cores=8)
+        run(script='solution4.py', tmax='1h', cores=8)
         run(script='solution5.py', tmax='1h', cores=8)
         with run(script='batteries2.py', tmax='3h'):
             run(script='batteries3.py', tmax='1h', cores=8)
