ae736a25ae2e0f800986ebe1079faaa9f2452ed1
the-ohm <mikhik@yahoo.dk>
2026-05-20 12:02:11 +0200

    Getting somewhere

--- gpaw/mixer/basemixer.py
+++ gpaw/mixer/basemixer.py
@@ -11,30 +11,38 @@ class BaseMixer:
     def __init__(self,
                  desc: DomainType):
         self.desc = desc
+        self.xp = self.desc.xp
         self.density_history = DensityHistory(
             nmaxold=1,
             nspins=desc.nspins,
             desc=desc,
-            atoms_layout=None,
         )
 
     def mix(self, density: Density) -> float:
-
         ntc_sR = add_compensation_charge(density)
+        if self.density_history.nold == 0:
+            self.density_history.add(ntc_sR)
+            return np.inf
+
         Rc_sR = self.calculate_residual(ntc_sR)
-        self.prev_density = density
+        self.density_history.add(ntc_sR)
         return self.calculate_charge_sloshing(Rc_sR)
 
     def calculate_charge_sloshing(self, res_sX: XArray) -> float:
-        return
+        slosh_sX = self.desc.empty()
+        slosh_sX.data[:] = self.xp.abs(res_sX.data)
+        return slosh_sX.integrate()
 
-    def calculate_residual(self, density: Density) -> Density:
-        if self.prev_density is None:
-            return density
-        return density - self.prev_density
+    def calculate_residual(self, ntc_sX: XArray, res_sX: XArray) -> XArray:
+        assert ntc_sX.data is not out_sX.data
+        # We do density history first, in case res_sX is the same as
+        # self.density_history[-1].
+        res_sX.data[:] = -self.density_history[-1].data
+        res_sX.data += ntc_sX.data
+        return res_sX
 
     def add_compensation_charge(self, density: Density) -> XArray:
-        return density
+        return density.nt_sR
 
     def __str__(self):
         return "No-mixing mixer"
@@ -44,7 +52,7 @@ class DensityHistory:
                  nmaxold: int,
                  nspins: int,
                  desc: DomainType,
-                 atoms_layout: AtomsArrayLayout | None):
+                 atoms_layout: AtomsArrayLayout | None = None):
         self.nmaxold = nmaxold
         self.nspins = nspins
         self.desc = desc
@@ -54,20 +62,17 @@ class DensityHistory:
             if atoms_layout is not None else None
         self.current_indicies = []
 
-    def add_density(self, density: Density, copy=True) -> None:
-        if copy:
-            self.add(density.n_sR.copy(), density.D_asii.copy())
-        else:
-            self.add(density.n_sR, density.D_asii)
+    def add_density(self, density: Density) -> None:
+        self.add(density.n_sR, density.D_asii)
 
     def add(self, n_sX: XArray,
             D_asii: AtomsArray | None = None) -> None:
         if self.nold == self.nmaxold:
             self.delete_oldest()
 
-        self._n_hsX.data[self.next_index] = n_sX
+        self._n_hsX.data[self.next_index] = n_sX.data
         if self._D_hasii is not None:
-            self._D_hasii.data[self.next_index] = D_asii
+            self._D_hasii.data[self.next_index] = D_asii.data
 
         self.current_indicies.append(self.next_index)
 
