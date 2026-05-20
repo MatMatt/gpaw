89ecd4f9345a75c1b9174c94bf7df1ae37496dcb
the-ohm <mikhik@yahoo.dk>
2026-05-20 21:45:51 +0200

    ...

--- gpaw/mixer/__init__.py
+++ gpaw/mixer/__init__.py
@@ -1,3 +1,18 @@
 # Imports for backwards compatibility:
-from gpaw.old.mixer import (Mixer, MixerSum, BroydenMixer,
-                            MixerFull, MixerDiff)
+from gpaw.old.mixer import _definemixerfunc
+
+
+Mixer = _definemixerfunc('separate', 'pulay')
+MixerSum = _definemixerfunc('sum', 'pulay')
+MixerSum2 = _definemixerfunc('sum2', 'pulay')
+MixerDif = _definemixerfunc('difference', 'pulay')
+MixerFull = _definemixerfunc('fullspin', 'pulay')
+FFTMixer = _definemixerfunc('separate', 'fft')
+FFTMixerSum = _definemixerfunc('sum', 'fft')
+FFTMixerSum2 = _definemixerfunc('sum2', 'fft')
+FFTMixerDif = _definemixerfunc('difference', 'fft')
+FFTMixerFull = _definemixerfunc('fullspin', 'fft')
+BroydenMixer = _definemixerfunc('separate', 'broyden')
+BroydenMixerSum = _definemixerfunc('sum', 'broyden')
+BroydenMixerSum2 = _definemixerfunc('sum2', 'broyden')
+BroydenMixerDif = _definemixerfunc('difference', 'broyden')
