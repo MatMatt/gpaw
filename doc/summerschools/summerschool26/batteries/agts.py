0daba859d22464492c1f401dd842311bee3b77f1
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-05-22 11:13:22 +0200

    ...

--- doc/summerschools/summerschool26/batteries/agts.py
+++ doc/summerschools/summerschool26/batteries/agts.py
@@ -12,10 +12,17 @@ def workflow():
             if not file.is_file():
                 shutil.copyfile(dir / file, file)
 
+    # batteries1:
     run(script='relax-graphite.py', tmax='1h')
     with run(script='solution3.py', tmax='1h', cores=8):
         run(script='solution4.py', tmax='1h', cores=8)
         run(script='solution5.py', tmax='1h', cores=8)
-        s21 = run(script='fepo4.py', tmax='1h', cores=8)
-        s22 = run(script='lifepo4.py', tmax='1h', cores=8)
-        run(script='batteries3.py', tmax='1h', cores=8, deps=[s21, s22])
+
+    # batteries2:
+    d1 = run(script='fepo4.py', tmax='1h', cores=8)
+    d2 = run(script='lifepo4.py', tmax='1h', cores=8)
+    d3 = run(script='li_metal.py', tmax='1h', cores=8)
+    run(script='eq_pot.py', deps=[d1, d2, d3])
+
+    # batteries3:
+    run(script='batteries3.py', tmax='1h', cores=8, deps=[s21, s22])
