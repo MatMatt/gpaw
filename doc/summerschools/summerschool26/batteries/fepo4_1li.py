d89eae5ad9cefd73ec8fc21b469f52ed8ddef707
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-05-27 13:44:34 +0200

    ...

---
+++ doc/summerschools/summerschool26/batteries/fepo4_1li.py
@@ -0,0 +1,34 @@
+from ase.io import read
+from gpaw import GPAW, PW
+from ase.dft.bee import BEEFEnsemble
+
+fepo4 = read('fepo4.traj')
+fepo4_1li = fepo4.copy()
+print(fepo4_1li.get_initial_magnetic_moments())
+# snippet-li
+fepo4_1li.append('Li')
+positions[-1] = (0, 0, 0)  # not really needed
+# snippet-gpaw
+params = dict(
+    mode=PW(500),
+    nbands=-40,
+    kpts={'size': (2, 4, 5),
+          'gamma': True},
+    spinpol=True,
+    xc='BEEF-vdW',
+    occupations=FermiDirac(width=0.1),
+    convergence={'eigenstates': 1.0e-4,
+                 'energy': 2.0e-4,
+                 'density': 1.0e-3},
+    mixer=Mixer(0.1, 5, weight=100.0),
+    setups={'Fe': ':d,4.3'}
+calc = GPAW(**params)
+fepo4_1li.calc = calc
+epot_fepo4_1li_cell = fepo4_1li.get_potential_energy()
+print('E_Pot=', epot_fepo4_1li_cell)
+write('fepo4_1li_out.traj', fepo4_1li)
+ens = BEEFEnsemble(calc)
+dE = ens.get_ensemble_energies(2000)
+with paropen('ensemble_fepo4_1li.dat', 'w') as fd:
+    for i in range(len(dE)):
+        print(dE[i], file=fd)
