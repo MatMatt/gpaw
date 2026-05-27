966ba9979e8105182ffbdf2e5c13b4e07de5e9a5
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-05-27 12:18:09 +0200

    Li barrier as a function of strain

--- doc/summerschools/summerschool26/batteries/agts.py
+++ doc/summerschools/summerschool26/batteries/agts.py
@@ -27,5 +27,5 @@ def workflow():
     run(script='eq_pot.py', deps=[d1, d2, d3])
 
     # batteries3:
-    run(script='li_barrier.py', tmax='1h', cores=8)
-    # run(script='li_barrier.py', tmax='1h', cores=8, deps=[d1, d2, d3])
+    with run(script='li_barrier.py', tmax='1h', cores=8):
+        run(script='li_barrier_2.py', tmax='1h', cores=8)
