5a946610940df40e7f10bb101b81d227a7a25e75
Tuomas Rossi <tuomas.rossi@csc.fi>
2026-06-01 13:02:01 +0300

    Increment cgpaw version

--- c/_gpaw.h
+++ c/_gpaw.h
@@ -443,7 +443,7 @@ static PyObject* moduleinit(void)
 #endif
 
     // Version number of C-code.  Keep in sync with gpaw/_broadcast_imports.py
-    PyObject_SetAttrString(m, "version", PyLong_FromLong(12));
+    PyObject_SetAttrString(m, "version", PyLong_FromLong(13));
 
     Py_INCREF(&LFCType);
     Py_INCREF(&OperatorType);
