beedd30ee7bc19945d27443c1911c9a2a3b2bc15
Tuomas Rossi <tuomas.rossi@csc.fi>
2026-06-01 12:52:45 +0300

    Set MPI backend to serial if no MPI support built

--- gpaw/__main__.py
+++ gpaw/__main__.py
@@ -4,7 +4,11 @@ from gpaw.cli.main import main
 # Set MPI backend to 'cgpaw' by default if executing
 # `gpaw` or `python -m gpaw`
 if gpaw.GPAW_MPI_BACKEND is None:
-    gpaw.GPAW_MPI_BACKEND = "cgpaw"
+    import gpaw.cgpaw as cgpaw
+    if cgpaw.have_mpi:
+        gpaw.GPAW_MPI_BACKEND = "cgpaw"
+    else:
+        gpaw.GPAW_MPI_BACKEND = "serial"
 
 if __name__ == '__main__':
     main()
