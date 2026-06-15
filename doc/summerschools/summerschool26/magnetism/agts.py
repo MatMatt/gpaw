4b2155ebc5f15e628d4d9433dde4dccce44d274a
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-06-15 15:30:52 +0200

    Add scripts to AGTS

--- doc/summerschools/summerschool26/magnetism/agts.py
+++ doc/summerschools/summerschool26/magnetism/agts.py
@@ -3,5 +3,12 @@ from myqueue.workflow import run
 
 def workflow():
     with run(script='xyz.py'):
-        run(script='magnetism1.py', tmax='1h')
-        run(script='magnetism2.py', tmax='2h')
+        run(script='CrI3_gs.py')
+        run(script='CrI3_fm.py')
+        run(script='get_Tc_mf.py')
+        run(script='get_Tc_fit.py')
+        run(script='CrI3_anisotropy.py')
+        run(script='VI2_gs.py')
+        run(script='VI2_afm.py')
+        run(script='VI2_noncol.py')
+        run(script='VI2_anisotropy.py')
