b25bad6953e2a0d853510fed895703bb0ec8ee2b
mnygp <markus.markusnp@gmail.com>
2026-06-15 15:52:20 +0200

    flake8

--- doc/summerschools/summerschool26/excitedstates/solution2.py
+++ doc/summerschools/summerschool26/excitedstates/solution2.py
@@ -1,9 +1,9 @@
+from ase.build import bulk
 atoms = bulk('Si', 'diamond', a=5.4)
 pw_cut_off = 300
 kpts_grid = (3, 3, 3)
 
 # snippet-pw-groundstate-start
-from ase.build import bulk
 from gpaw import GPAW, FermiDirac
 from gpaw import PW
 
@@ -22,17 +22,17 @@ calc.diagonalize_full_hamiltonian()  # determine all bands
 calc.write('Si_groundstate.gpw', 'all')
 # snippet-pw-groundstate-end
 
-nbands=30,# number of bands for calculation of self-energy
-bands=(3, 5),# VB and CB
-ecut=20.0, # plane-wave cutoff for self-energy (20-200)
+nbands = 30,  # number of bands for calculation of self-energy
+bands = (3, 5),  # VB and CB
+ecut = 20.0,  # plane-wave cutoff for self-energy (20-200)
 
 # snippet-pw-g0w0-start
 from gpaw.response.g0w0 import G0W0
 
 gw = G0W0(calc='Si_groundstate.gpw',
-          nbands=nbands, # number of bands for calculation of self-energy
-          bands=bands, # VB and CB
-          ecut=ecut, # plane-wave cutoff for self-energy (20-200)
+          nbands=nbands,  # number of bands for calculation of self-energy
+          bands=bands,  # VB and CB
+          ecut=ecut,  # plane-wave cutoff for self-energy (20-200)
           integrate_gamma='WS',  # Use supercell Wigner-Seitz truncation for W.
           filename='Si-g0w0')
 gw.calculate()
@@ -41,17 +41,17 @@ gw.calculate()
 # snippet-pw-direct-gap-start
 import pickle
 
-results = pickle.load(open('Si-g0w0_results_GW.pckl', 'rb')) # student: open('???-g0w0_results_GW.pckl', 'rb')
+results = pickle.load(open('Si-g0w0_results_GW.pckl', 'rb'))
 direct_gap = results['qp'][0, 0, -1] - results['qp'][0, 0, -2]
 
-print('Direct bandgap of Si:', direct_gap) # student: 'Direct bandgap of ???:', direct_gap
+print('Direct bandgap of Si:', direct_gap)
 # snippet-pw-direct-gap-end
 
 
 # snippet-lcao-groundstate-start
-calc = GPAW(mode="lcao",  # student mode lcao
-            basis="dzp",  # basis set dzp
-            nbands="nao", # nbands nao
+calc = GPAW(mode="lcao",   # student mode lcao
+            basis="dzp",   # basis set dzp
+            nbands="nao",  # nbands nao
             kpts={'size': kpts_grid, 'gamma': True},
             xc='LDA',
             occupations=FermiDirac(0.001),
@@ -71,23 +71,23 @@ dft.change_mode('pw')
 dft.write_gpw_file('Si_pw_from_lcao_groundstate.gpw', include_wfs=True)
 # snippet-lcao-to-pw-end
 
-nbands=26
-bands=(3, 5)
-ecut=20.0
+nbands = 26
+bands = (3, 5)
+ecut = 20.0
 
 # snippet-lcao-g0w0-start
 gw = G0W0(calc='Si_pw_from_lcao_groundstate.gpw',
-          nbands=nbands, # number of bands for calculation of self-energy
-          bands=bands,# VB and CB
-          ecut=ecut,# plane-wave cutoff for self-energy (20-200)
+          nbands=nbands,  # number of bands for calculation of self-energy
+          bands=bands,  # VB and CB
+          ecut=ecut,  # plane-wave cutoff for self-energy (20-200)
           integrate_gamma='WS',  # Use supercell Wigner-Seitz truncation for W.
-          filename='Si-from-lcao-g0w0') 
+          filename='Si-from-lcao-g0w0')
 gw.calculate()
 # snippet-lcao-g0w0-end
 
 # snippet-lcao-direct-gap-start
-results = pickle.load(open('Si-from-lcao-g0w0_results_GW.pckl', 'rb')) 
+results = pickle.load(open('Si-from-lcao-g0w0_results_GW.pckl', 'rb'))
 direct_gap = results['qp'][0, 0, -1] - results['qp'][0, 0, -2]
 
-print('Direct bandgap of Si:', direct_gap) 
+print('Direct bandgap of Si:', direct_gap)
 # snippet-lcao-direct-gap-end
