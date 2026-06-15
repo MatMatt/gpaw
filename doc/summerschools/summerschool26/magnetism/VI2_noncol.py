ccda90166f2bc037ed41345cdfe48833886892f6
Thomas Olsen <tolsen@fysik.dtu.dk>
2026-06-15 12:51:40 +0200

    Forgot one file

---
+++ doc/summerschools/summerschool26/magnetism/VI2_noncol.py
@@ -0,0 +1,34 @@
+import numpy as np
+from ase.io import read
+from ase.visualize import view
+from gpaw import GPAW, PW, MixerDif
+    
+m = 3
+cell_cv = layer.get_cell()
+layer_nc = layer.repeat((3, 1, 1))
+new_cell_cv = [[3 * cell_cv[0, 0] / 2, 3**0.5 * cell_cv[0, 0] / 2, 0.0],
+               [3 * cell_cv[0, 0] / 2, -3**0.5 * cell_cv[0, 0] / 2, 0.0],
+               [0.0, 0.0, cell_cv[2, 2]]]
+layer_nc.set_cell(new_cell_cv)
+view(layer_nc)
+
+magmoms = np.zeros((len(layer_nc), 3), float)
+magmoms[0] = [m, 0, 0]
+magmoms[3] = [m * np.cos(2 * np.pi / 3), m * np.sin(2 * np.pi / 3), 0]
+magmoms[6] = [m * np.cos(2 * np.pi / 3), -m * np.sin(2 * np.pi / 3), 0]
+
+calc = GPAW(mode=PW(400),
+            xc='LDA',
+            mixer=MixerDif(),
+            symmetry='off',
+            magmoms=magmoms,
+            soc=False,
+            parallel={'domain': 1, 'band': 1},
+            kpts=(2, 2, 1))
+layer_nc.calc = calc
+layer_nc.get_potential_energy()
+calc.write('nc_nosoc.gpw')
+
+# ...
+# Include similar calculations here for a ferromagnetic state using
+# the non-collinear framework
