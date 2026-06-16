99f06de7a5c6c83cf839c7723aaa5bfb20ee45be
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-06-16 09:57:24 +0200

    Simplify G2-1 test

--- gpaw/test/big/g2_1/agts.py
+++ gpaw/test/big/g2_1/agts.py
@@ -1,6 +1,11 @@
 from myqueue.workflow import run
+from ase.data.g2_1 import atom_names, molecule_names
+from gpaw.test.big.g2_1.g21gpaw import relax
 
 
 def workflow():
-    with run(script='g21gpaw.py', tmax='20h'):
-        run(script='analyse.py')
+    deps = []
+    for name in molecule_names + atom_names:
+        d = run(function=relax)
+        deps.apend(d)
+    run(script='analyse.py', deps=deps)
