c9510d7031f35401555676cdef77dd0e0e44380f
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-06-16 10:05:39 +0200

    TYPOS

--- gpaw/test/big/g2_1/agts.py
+++ gpaw/test/big/g2_1/agts.py
@@ -6,6 +6,6 @@ from gpaw.test.big.g2_1.g21gpaw import relax
 def workflow():
     deps = []
     for name in molecule_names + atom_names:
-        d = run(function=relax)
-        deps.apend(d)
+        d = run(function=relax, args=[name])
+        deps.append(d)
     run(script='analyse.py', deps=deps)
