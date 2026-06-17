96638d62d9f27d466e6485d93a9251d19fdd808a
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-06-17 10:44:19 +0200

    ...

--- doc/summerschools/summerschool26/catalysis/check_convergence.py
+++ doc/summerschools/summerschool26/catalysis/check_convergence.py
@@ -1,24 +1,28 @@
-# web-page: convergence.db
-import numpy as np
+# web-page: results.json
+import json
+from pathlib import Path
+
 from ase import Atoms
 from ase.build import hcp0001
 from ase.constraints import FixAtoms
 from ase.optimize import BFGSLineSearch
-from gpaw import GPAW, PW, Davidson
+from gpaw import GPAW, PW
+from gpaw.mpi import world
 
 a = 2.72
 c = 1.58 * a
 vacuum = 4.0
 
 
-def adsorb(db, height=1.2, nlayers=3, nkpts=7, ecut=400):
+def adsorb(height: float = 1.2,
+           nlayers: int = 3,
+           nkpts: int = 7,
+           ecut: float = 400) -> tuple[float, float, float]:
     """Adsorb nitrogen in hcp-site on Ru(0001) surface.
 
     Do calculations for N/Ru(0001), Ru(0001) and a nitrogen atom
     if they have not already been done.
 
-    db: Database
-        Database for collecting results.
     height: float
         Height of N-atom above top Ru-layer.
     nlayers: int
@@ -29,21 +33,21 @@ def adsorb(db, height=1.2, nlayers=3, nkpts=7, ecut=400):
     ecut: float
         Cutoff energy for plane waves.
 
-    Returns height.
+    Returns height, energy of N/Ru(0001) and clean Ru(0001).
     """
 
     name = f'Ru{nlayers}-{nkpts}x{nkpts}-{ecut:.0f}'
 
-    parameters = dict(mode=PW(ecut),
-                      eigensolver=Davidson(niter=2),
-                      poissonsolver={'dipolelayer': 'xy'},
-                      kpts={'size': (nkpts, nkpts, 1), 'gamma': True},
-                      xc='PBE')
+    parameters = dict(
+        mode=PW(ecut),
+        poissonsolver={'dipolelayer': 'xy'},
+        kpts={'size': (nkpts, nkpts, 1), 'gamma': True},
+        xc='PBE')
 
     # N/Ru(0001):
     slab = hcp0001('Ru', a=a, c=c, size=(1, 1, nlayers))
     z = slab.positions[:, 2].max() + height
-    x, y = np.dot([2 / 3, 2 / 3], slab.cell[:2, :2])
+    x, y = [2 / 3, 2 / 3] @ slab.cell[:2, :2]
     slab.append('N')
     slab.positions[-1] = [x, y, z]
     slab.center(vacuum=vacuum, axis=2)  # 2: z-axis
@@ -51,58 +55,71 @@ def adsorb(db, height=1.2, nlayers=3, nkpts=7, ecut=400):
     # Fix first nlayer atoms:
     slab.constraints = FixAtoms(indices=list(range(nlayers)))
 
-    id = db.reserve(name=f'N/{nlayers}Ru(0001)', nkpts=nkpts, ecut=ecut)
-    if id is not None:  # skip calculation if already done
-        slab.calc = GPAW(txt='N' + name + '.txt',
-                         **parameters)
-        optimizer = BFGSLineSearch(slab, logfile='N' + name + '.opt')
-        optimizer.run(fmax=0.01)
-        height = slab.positions[-1, 2] - slab.positions[:-1, 2].max()
-        del db[id]
-        db.write(slab,
-                 name=f'N/{nlayers}Ru(0001)', nkpts=nkpts, ecut=ecut,
-                 height=height)
+    slab.calc = GPAW(txt=f'N{name}.txt',
+                     **parameters)
+    optimizer = BFGSLineSearch(slab, logfile=f'N{name}.opt')
+    optimizer.run(fmax=0.01)
+    height = slab.positions[-1, 2] - slab.positions[:-1, 2].max()
+    enru = slab.get_potential_energy()
 
     # Clean surface (single point calculation):
-    id = db.reserve(name=f'{nlayers}Ru(0001)', nkpts=nkpts, ecut=ecut)
-    if id is not None:
-        del slab[-1]  # remove nitrogen atom
-        slab.calc = GPAW(txt=name + '.txt',
-                         **parameters)
-        slab.get_forces()
-        del db[id]
-        db.write(slab,
-                 name=f'{nlayers}Ru(0001)', nkpts=nkpts, ecut=ecut)
+    del slab[-1]  # remove nitrogen atom
+    slab.calc = GPAW(txt=f'{name}.txt',
+                     **parameters)
+    eru = slab.get_potential_energy()
 
-    # Nitrogen atom:
-    id = db.reserve(name='N-atom', ecut=ecut)
-    if id is not None:
-        # Create spin-polarized nitrogen atom:
-        molecule = Atoms('N', magmoms=[3])
-        molecule.center(vacuum=4.0)
-        # Remove parameters that make no sense for an isolated atom:
-        del parameters['kpts']
-        del parameters['poissonsolver']
-        # Calculate energy:
-        molecule.calc = GPAW(txt=name + '.txt', **parameters)
-        molecule.get_potential_energy()
-        del db[id]
-        db.write(molecule, name='N-atom', ecut=ecut)
-
-    return height
+    return height, enru, eru
 
 
-def run():
-    h = 1.2
-    from ase.db import connect
-    db = connect('convergence.db')
-    for n in range(1, 10):
-        h = adsorb(db, h, n, 7, 400)
-    for k in range(4, 18):
-        h = adsorb(db, h, 2, k, 400)
+def atom(ecut: float = 400) -> float:
+    """Calculate energy of spin-polarized nitrogen atom."""
+    molecule = Atoms('N', magmoms=[3])
+    molecule.center(vacuum=4.0)
+    parameters = dict(
+        mode=PW(ecut),
+        xc='PBE')
+    name = f'N-{ecut:.0f}'
+    molecule.calc = GPAW(txt=f'{name}.txt', **parameters)
+    en = molecule.get_potential_energy()
+    return en
+
+
+def main():
+    h = 1.2  # initial guess
+
+    # N-atom energies:
+    eatom = {}
     for ecut in range(350, 801, 50):
-        h = adsorb(db, h, 2, 7, ecut)
+        en = atom(ecut)
+        eatom[ecut] = en
+
+    # Save height and adsorption energy in a dict where the
+    # key is a tuple of layers, k-point and ecut:
+    results: dict[tuple[int, int, int], tuple[float, float]] = {}
+
+    # ecut convergence:
+    for ecut in range(350, 801, 50):
+        h, enru, eru = adsorb(h, 2, 7, ecut)
+        results[(2, 7, ecut)] = (h, enru - eru - eatom[ecut])
+
+    # Number of layers convergence:
+    for n in range(1, 10):
+        if n == 2:
+            continue  # already done
+        h, enru, eru = adsorb(h, n, 7, 400)
+        results[(n, 7, 400)] = (h, enru - eru - eatom[ecut])
+
+    # Number of k-points convergence:
+    for k in range(4, 18):
+        if k == 7:
+            continue  # already done
+        h, enru, eru = adsorb(h, 2, k, 400)
+        results[(2, k, 400)] = (h, enru - eru - eatom[ecut])
+
+    if world.rank == 0:
+        Path('result.json').write_text(
+            json.dumps(results, indent=1))
 
 
 if __name__ == '__main__':
-    run()
+    main()
