0694afc8bffc4faa7169412d27697db95c6638b2
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-06-17 12:46:08 +0200

    AGTS...

--- doc/summerschools/summerschool26/catalysis/convergence/agts.py
+++ doc/summerschools/summerschool26/catalysis/convergence/agts.py
@@ -2,5 +2,6 @@ from myqueue.workflow import run
 
 
 def workflow():
-    with run(script='check_convergence.py', tmax='1h', cores=8):
+    with run(script='eads.py', tmax='1h', cores=8)
+    with run(script='convergence.py', tmax='1h', cores=8):
         run(script='plot.py')
