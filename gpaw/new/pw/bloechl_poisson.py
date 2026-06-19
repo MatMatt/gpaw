5bd43a9c5b72d4c05f4cf4734e5b42afc3b0fcb4
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-06-19 14:52:39 +0200

    Polish test

--- gpaw/new/pw/bloechl_poisson.py
+++ gpaw/new/pw/bloechl_poisson.py
@@ -216,7 +216,7 @@ class BloechlPAWPoissonSolver(PAWPoissonSolver):
         e_coulomb = comm.sum_scalar(e_coulomb1 / comm.size +
                                     e_coulomb2 +
                                     e_coulomb3)
-        self.de_stress = -(0*e_coulomb2 + e_coulomb3)
+        self.de_stress = -(0 * e_coulomb2 + e_coulomb3)
 
         return e_coulomb, vHt_g, V_aL
 
@@ -262,9 +262,9 @@ class BloechlPAWPoissonSolver(PAWPoissonSolver):
         _, pair_pot_stress_vv = self._force_and_stress(Q_aL)
         v_vv = self.vhat_aLg.stress_contribution(nt_g, Q_aL)
         g_vv = self.ghat_aLg.stress_contribution(vHt_g, Q_aL)
-        from ase.units import Ha, Bohr
-        print(np.trace(pair_pot_stress_vv) * Ha / Bohr**3 / self.pwg.volume)
-        print(np.trace(v_vv) * Ha / Bohr**3 / self.pwg.volume)
-        print(np.trace(g_vv) * Ha / Bohr**3 / self.pwg.volume)
-        print(self.de_stress * Ha / Bohr**3 / self.pwg.volume)
+        # from ase.units import Ha, Bohr
+        # print(np.trace(pair_pot_stress_vv) * Ha / Bohr**3 / self.pwg.volume)
+        # print(np.trace(v_vv) * Ha / Bohr**3 / self.pwg.volume)
+        # print(np.trace(g_vv) * Ha / Bohr**3 / self.pwg.volume)
+        # print(self.de_stress * Ha / Bohr**3 / self.pwg.volume)
         return v_vv + g_vv + pair_pot_stress_vv - self.de_stress * np.eye(3)
